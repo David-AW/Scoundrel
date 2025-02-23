fdd7eedb116c536fa0c5c5e025eab9d89e2656f2
david-aw <30061695+David-AW@users.noreply.github.com>
2025-02-23 17:25:31 -0500

    Implement Card data type

--- src/com/davidaw/scoundrel/Card.java
+++ src/com/davidaw/scoundrel/Card.java
@@ -1,8 +1,26 @@
 package com.davidaw.scoundrel;
 
-public class Card {
+public final class Card implements Comparable<Card>{
 
-	private CardType type;
-	private int value;
+	private final CardType type;
+	private final int value;
+	
+	public Card(CardType type, int value) {
+		this.type = type;
+		this.value = value;
+	}
+	
+	public CardType getType() {
+		return type;
+	}
+	
+	public int getValue() {
+		return value;
+	}
+
+	@Override
+	public int compareTo(Card other) {
+		return this.value - other.value;
+	}
 	
 }
