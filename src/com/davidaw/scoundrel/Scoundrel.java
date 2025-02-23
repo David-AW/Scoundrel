99251497262a009cd77142a052c856e77d269299
david-aw <30061695+David-AW@users.noreply.github.com>
2025-02-23 03:12:06 -0500

    Initial commit

---
+++ src/com/davidaw/scoundrel/Scoundrel.java
@@ -0,0 +1,15 @@
+package com.davidaw.scoundrel;
+
+/**
+ * Quick and easy programming project out of boredom.
+ * Based on the playing card rouge-like "Scoundrel"
+ * @author david-aw
+ */
+
+public class Scoundrel {
+
+	public Scoundrel() {
+		
+	}
+	
+}
