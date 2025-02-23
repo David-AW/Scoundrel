04884f1298afdfbd8c74e2308b5ea0bf044fe87e
david-aw <30061695+David-AW@users.noreply.github.com>
2025-02-23 18:13:37 -0500

    Populate card pool in constructor

--- src/com/davidaw/scoundrel/Deck.java
+++ src/com/davidaw/scoundrel/Deck.java
@@ -4,4 +4,19 @@ public class Deck {
 
 	private Card[] pool;
 	
+	public Deck() {
+		// 26 monster cards, 9 weapon cards, 9 potion cards
+		pool = new Card[44];
+
+		for (int i = 0; i < 13; i++) {
+			pool[i*2] = new Card(CardType.MONSTER, i+2);
+			pool[i*2+1] = new Card(CardType.MONSTER, i+2);
+		}
+
+		for (int i = 0; i < 9; i++) {
+			pool[i+26] = new Card(CardType.WEAPON, i+2);
+			pool[i+35] = new Card(CardType.POTION, i+2);
+		}
+	}
+	
 }
