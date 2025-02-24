1839ad3de346b50c61fc34d32898e045165a8ccf
david-aw <30061695+David-AW@users.noreply.github.com>
2025-02-23 23:38:40 -0500

    Implement two shuffling functions

--- src/com/davidaw/scoundrel/Deck.java
+++ src/com/davidaw/scoundrel/Deck.java
@@ -1,12 +1,19 @@
 package com.davidaw.scoundrel;
 
-public class Deck {
+import java.util.Random;
+import java.util.Stack;
 
-	private Card[] pool;
+public class Deck extends ArrayQueue<Card>{
+	
+	private Random random;
 	
 	public Deck() {
+		super(44);
+		
+		random = new Random();
+		
 		// 26 monster cards, 9 weapon cards, 9 potion cards
-		pool = new Card[44];
+		Card[] pool = new Card[44];
 
 		for (int i = 0; i < 13; i++) {
 			pool[i*2] = new Card(CardType.MONSTER, i+2);
@@ -16,6 +23,38 @@ public class Deck {
 		for (int i = 0; i < 9; i++) {
 			pool[i+26] = new Card(CardType.WEAPON, i+2);
 			pool[i+35] = new Card(CardType.POTION, i+2);
+		}
+		
+		fill(pool);
+	}
+	
+	public void shuffle() {
+		floorShuffle();
+		splitShuffle();
+		floorShuffle();
+	}
+	
+	private void floorShuffle() {
+		for (int i = 0; i < size(); i++) {
+			swap(random.nextInt(0, maxSize()), i);
+		}
+	}
+	
+	private void splitShuffle() {
+		Stack<Card> a = new Stack<Card>();
+		Stack<Card> b = new Stack<Card>();
+		Card[] cards = toArray();
+		for (int i = 0; i < cards.length; i++) {
+			if (i < cards.length/2)
+				a.push(cards[i]);
+			else
+				b.push(cards[i]);
+		}
+		clear();
+		while(!b.isEmpty()) {
+			enqueue(b.pop());
+			if (!a.isEmpty())
+				enqueue(a.pop());
 		}
 	}
 	
