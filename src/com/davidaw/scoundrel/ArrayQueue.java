c0f36be72549d6242ae08069f21c55354a025e10
david-aw <30061695+David-AW@users.noreply.github.com>
2025-02-23 20:36:18 -0500

    Implement an ArrayQueue data type

---
+++ src/com/davidaw/scoundrel/ArrayQueue.java
@@ -0,0 +1,88 @@
+package com.davidaw.scoundrel;
+public class ArrayQueue<T> {
+
+	Object[] storage;
+	
+	int start = 0;
+	int next = 0;
+	
+	public ArrayQueue() {
+		this(16);
+	}
+	
+	public ArrayQueue(int queue_size) {
+		storage = new Object[queue_size];
+	}
+	
+	public boolean enqueue(T e) {
+		if (isFull())
+			return false;
+		storage[next++ % storage.length] = e;
+		return true;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public T dequeue() {
+		if (isEmpty())
+			return null;
+		T temp = (T) storage[start++];
+		// This is to avoid the inevitable possibility of running out of bits in the integer
+		if (start >= storage.length) {
+			next -= start;
+			start = 0;
+		}
+		return temp;
+	}
+
+	public boolean swap(int from_index, int to_index) {
+		int normalized_next = next - start;
+		if (from_index < 0 || from_index >= to_index || to_index > normalized_next)
+			return false;
+		int actual_from = (from_index + next) % storage.length;
+		int actual_to = (to_index + next) % storage.length;
+		Object temp = storage[actual_to];
+		storage[actual_to] = storage[actual_from];
+		storage[actual_from] = temp;
+		return true;
+	}
+	
+	public void fill(T[] list) {
+		for(T item : list) {
+			if (isFull())
+				break;
+			enqueue(item);
+		}
+	}
+
+	public boolean isEmpty() {
+		return start == next;
+	}
+
+	public boolean isFull() {
+		return size() >= storage.length;
+	}
+
+	public int size() {
+		return next-start;
+	}
+	
+	public int maxSize() {
+		return storage.length;
+	}
+
+	public String toString(String delimiter) {
+		String temp = "";
+		for (int i = start; i < next; i++) {
+			temp += storage[i%storage.length].toString();
+			if (i < next-1)
+				temp += delimiter;
+		}
+		return temp;
+	}
+	
+	@Override
+	public String toString() {
+		return toString("");
+	}
+	
+}
